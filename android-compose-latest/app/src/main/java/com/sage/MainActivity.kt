8a638edcf06cf3f68bdef7c03e9bab0fee73b9db
Freedom <979263784@qq.com>
2023-04-21 17:19:47 +0800

    android-compose-latest 新增一些依赖

--- android-compose-latest/app/src/main/java/com/sage/MainActivity.kt
+++ android-compose-latest/app/src/main/java/com/sage/MainActivity.kt
@@ -3,14 +3,31 @@ package com.sage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
 import com.sage.ui.theme.AndroidcomposelatestTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,15 +43,71 @@ class MainActivity : ComponentActivity() {
   }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String) {
-  Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  AndroidcomposelatestTheme {
-    Greeting("Android")
+  var openBottomSheet by rememberSaveable { mutableStateOf(false) }
+  var skipPartiallyExpanded by remember { mutableStateOf(false) }
+  val scope = rememberCoroutineScope()
+  val bottomSheetState = rememberSheetState()
+
+// App content
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+  ) {
+    Row(
+      Modifier.toggleable(
+        value = skipPartiallyExpanded,
+        role = Role.Checkbox,
+        onValueChange = { checked -> skipPartiallyExpanded = checked }
+      )
+    ) {
+      Checkbox(checked = skipPartiallyExpanded, onCheckedChange = null)
+      Spacer(Modifier.width(16.dp))
+      Text("Skip partially expanded State")
+    }
+    Button(onClick = { openBottomSheet = !openBottomSheet }) {
+      Text(text = "Show Bottom Sheet")
+    }
+  }
+
+// Sheet content
+  if (openBottomSheet) {
+    ModalBottomSheet(
+      onDismissRequest = { openBottomSheet = false },
+      sheetState = bottomSheetState,
+    ) {
+      Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Button(
+          // Note: If you provide logic outside of onDismissRequest to remove the sheet,
+          // you must additionally handle intended state cleanup, if any.
+          onClick = {
+            scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
+              if (!bottomSheetState.isVisible) {
+                openBottomSheet = false
+              }
+            }
+          }
+        ) {
+          Text("Hide Bottom Sheet")
+        }
+      }
+      LazyColumn {
+        items(50) {
+          ListItem(
+            headlineText = { Text("Item $it") },
+            leadingContent = {
+              Icon(
+                Icons.Default.Favorite,
+                contentDescription = "Localized description"
+              )
+            }
+          )
+        }
+      }
+    }
   }
 }
