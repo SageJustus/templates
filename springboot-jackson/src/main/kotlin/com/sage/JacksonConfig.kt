2ec5ca90909bebe60c39acbbbe12ee1b317212a7
Freedom <979263784@qq.com>
2023-05-16 21:09:39 +0800

    jackson kotlin Long to String Custom Serializer

--- springboot-jackson/src/main/kotlin/com/sage/JacksonConfig.kt
+++ springboot-jackson/src/main/kotlin/com/sage/JacksonConfig.kt
@@ -1,8 +1,7 @@
 package com.sage
 
-import com.fasterxml.jackson.annotation.JsonFormat.Feature
-import com.fasterxml.jackson.core.json.JsonWriteFeature
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,8 +13,7 @@ class JacksonConfig {
     fun jackson2ObjectMapperBuilderCustomizer(): Jackson2ObjectMapperBuilderCustomizer =
         Jackson2ObjectMapperBuilderCustomizer { builder ->
             builder.serializerByType(Long::class.java, ToStringSerializer.instance)
-            //  https://github.com/FasterXML/jackson-core/issues/586#issuecomment-783187195
-//            builder.featuresToEnable(JsonWriteFeature.WRITE_NUMBERS_AS_STRINGS.mappedFeature())
+            builder.serializerByType(Long::class.javaObjectType, ToStringSerializer.instance)
         }
 
 }
